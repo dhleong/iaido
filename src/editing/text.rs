962cbf16ad3b5137d98cc2c37576690d7aab5928
Daniel Leong <me@dhleong.net>
2021-01-31 19:14:09 -0500

    Fix starts_with check

--- src/editing/text.rs
+++ src/editing/text.rs
@@ -20,8 +20,15 @@ impl EditableLine for TextLine {
     fn starts_with(&self, s: &String) -> bool {
         let mut index = 0;
         for span in &self.0 {
-            if span.content != s[index..] {
-                return false;
+            for i in 0..span.content.len() {
+                let desti = index + i;
+                if desti >= s.len() {
+                    return true;
+                }
+
+                if span.content[i..i + 1] != s[desti..desti + 1] {
+                    return false;
+                }
             }
 
             index += span.content.len();
@@ -105,6 +112,36 @@ mod tests {
             span.append(&mut part2);
             span.append(&mut part3);
             assert_eq!(span.subs(3, 10).to_string(), "e my lo");
+        }
+    }
+
+    #[cfg(test)]
+    mod starts_with {
+        use super::*;
+
+        #[test]
+        fn single_span() {
+            let s: String = "serenity".into();
+            let part1: TextLine = s.clone().into();
+            assert_eq!(part1.starts_with(&s), true);
+        }
+
+        #[test]
+        fn multi_span_equal_len() {
+            let expected: String = "serenity".into();
+            let mut line: TextLine = "sere".into();
+            let mut part2: TextLine = "nity".into();
+            line.append(&mut part2);
+            assert_eq!(line.starts_with(&expected), true);
+        }
+
+        #[test]
+        fn multi_span_shorter() {
+            let expected: String = "se".into();
+            let mut line: TextLine = "sere".into();
+            let mut part2: TextLine = "nity".into();
+            line.append(&mut part2);
+            assert_eq!(line.starts_with(&expected), true);
         }
     }
 }

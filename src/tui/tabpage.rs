a0d4683582cca5b07557e2494d406c0c2b342d02
Daniel Leong <me@dhleong.net>
2021-02-21 23:05:58 -0500

    Prepare initial tabpage rendering test harness

--- src/tui/tabpage.rs
+++ src/tui/tabpage.rs
@@ -10,3 +10,86 @@ impl Renderable for Tabpage {
         self.layout.render(app);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use tui::layout::Rect;
+
+    use crate::{
+        app,
+        editing::{buffers::Buffers, motion::tests::window, text::TextLines, Resizable, Size},
+        tui::{Display, RenderContext},
+    };
+
+    use super::*;
+
+    use crate::tui::rendering::display::tests::TestableDisplay;
+    use indoc::indoc;
+
+    pub struct TestableTabpage {
+        pub tab: Tabpage,
+        pub buffers: Buffers,
+        pub size: Size,
+    }
+
+    pub fn tabpage(s: &'static str) -> TestableTabpage {
+        let window = window(s);
+        let Size { w, h } = window.window.size;
+        let mut page = tabpage_of_size(w, h);
+
+        let id = page.buffers.create().id();
+        page.buffers
+            .by_id_mut(id)
+            .unwrap()
+            .append(TextLines::raw(s.replace("|", "")));
+        page.tab.current_window_mut().cursor = window.window.cursor;
+        page.tab.current_window_mut().buffer = id;
+
+        page
+    }
+
+    pub fn tabpage_of_size(w: u16, h: u16) -> TestableTabpage {
+        let mut buffers = Buffers::new();
+        let size = Size { w, h };
+        TestableTabpage {
+            tab: Tabpage::new(0, &mut buffers, size),
+            buffers,
+            size,
+        }
+    }
+
+    impl TestableTabpage {
+        pub fn render(mut self) -> Display {
+            let mut state = app::State::default();
+            state.buffers = self.buffers;
+
+            let area: Rect = self.size.into();
+            let mut display = Display::new(area.into());
+            let mut context = RenderContext {
+                app: &state,
+                display: &mut display,
+                area,
+                buffer_override: None,
+            };
+            self.tab.resize(self.size);
+            self.tab.render(&mut context);
+
+            display
+        }
+    }
+
+    #[test]
+    fn vertical_test() {
+        let mut tabpage = tabpage(indoc! {"
+            Take my love
+        "});
+        tabpage.tab.hsplit();
+        tabpage.size = Size { w: 12, h: 3 };
+
+        tabpage.render().assert_visual_equals(indoc! {"
+            Take my love
+            ────────────
+            Take my love
+        "});
+    }
+}

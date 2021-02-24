b3c56d443ec41b7df7816cd61e1afb37aa152e0c
Daniel Leong <me@dhleong.net>
2021-02-24 08:32:58 -0500

    Add basic ConnLayout rendering
    We'd still like to auto-resize it...

--- src/tui/layout/conn.rs
+++ src/tui/layout/conn.rs
@@ -11,6 +11,12 @@ impl Renderable for ConnLayout {
     }
 
     fn render(&self, ctx: &mut RenderContext) {
-        todo!()
+        let mut layout_area = ctx.area.clone();
+        layout_area.height = self.output.size.h;
+        self.output.render(&mut ctx.with_area(layout_area));
+
+        layout_area.y += self.output.size.h;
+        layout_area.height = self.input.size.h;
+        self.input.render(&mut ctx.with_area(layout_area));
     }
 }

e34752412832936ba899c64971d9b2e003e3c94f
Daniel Leong <me@dhleong.net>
2021-04-28 10:51:56 -0400

    Remove commented-out code structure planning
    It's all done! Don't need it anymore :)

--- src/script/api/core2.rs
+++ src/script/api/core2.rs
@@ -41,44 +41,8 @@ impl CurrentObjects {
         context.state().current_buffer().id()
     }
 
-    // expands to:
-    // pub fn buffer_id(&self) -> Id {
-    //   match self.api.perform(CurrentObjectsApi, CurrentObjectsApiRequest::buffer_id) {
-    //      Ok(CurrentObjectsApiResponse::buffer_id(id)) => id,
-    //      Ok(unexpected) => panic!("Unexpected response: {:?}", unexpected),
-    //      Err(e) => std::panic::panic_any(e),
-    //   }
-    // }
-    // ...
-    // enum CurrentObjectsApiRequest {
-    //   buffer_id,
-    // }
-    // enum CurrentObjectsApiResponse {
-    //   buffer_id(Id),
-    // }
-    // struct CurrentObjectsApi;
-    // impl ApiHandler<CurrentObjectsApiRequest> for CurrentObjectsApi {
-    //  fn handle(
-    //    &self,
-    //    context: &mut CommandHandlerContext,
-    //    p: CurrentObjectsApiRequest
-    //  ) -> ApiResult {
-    //    match p {
-    //      CurrentObjectsApiRequest::buffer_id => {
-    //        Ok(
-    //         CurrentObjectsApiResponse::buffer_id(
-    //           CurrentObjects::buffer_id(context),
-    //         )
-    //        )
-    //      }
-    //    }
-    //  }
-    // }
-
     #[property]
     pub fn buffer(&self) -> BufferApiObject {
-        // NOTE: buffer_id should be turned into a method that
-        // delegates to an RPC call
         BufferApiObject::new(self.api.clone(), self.buffer_id())
     }
 }
@@ -116,36 +80,4 @@ impl BufferApiObject {
             None
         }
     }
-
-    // expands to:
-    // pub fn name(&self) -> KeyResult<Option<String>> {
-    //     match self.api
-    //         .perform(BufferApiObjectApi, BufferApiObjectApiRequeset::name(self.id)) {
-    //       Ok(BufferApiObjectApiResponse::name(response)) => Ok(response),
-    //       Ok(unexpected) => panic!("Unexpected response: {:?}"),
-    //       Err(e) => Err(e)
-    //     }
-    // }
-    // ...
-    // enum BufferApiObjectApiRequest {
-    //   name(Id),
-    // }
-    // enum BufferApiObjectApiResponse {
-    //   name(Option<String>),
-    // }
-    // struct BufferApiObjectApi;
-    // impl ApiHandler<BufferApiObjectApiRequest> for BufferApiObjectApi {
-    //  fn handle(
-    //    &self,
-    //    context: &mut CommandHandlerContext,
-    //    p: BufferApiObjectApiRequest
-    //  ) -> ApiResult {
-    //    match p {
-    //      BufferApiObjectApiRequest::name(id) => {
-    //        let result = BufferApiObject::name(context, id);
-    //        Ok(result)
-    //      }
-    //    }
-    //  }
-    // }
 }

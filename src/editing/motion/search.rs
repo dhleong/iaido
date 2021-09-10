95a129616b6d28f83b50e66e0ea851f831194024
Daniel Leong <me@dhleong.net>
2021-09-10 09:03:59 -0400

    Prepare some search tests; fix infinite loop when not found

--- src/editing/motion/search.rs
+++ src/editing/motion/search.rs
@@ -13,17 +13,17 @@ pub struct SearchMotion {
 }
 
 impl SearchMotion {
-    pub fn backward_until(query: String) -> Self {
+    pub fn backward_until<T: Into<String>>(query: T) -> Self {
         Self {
             step: LineCrossing::new(CharMotion::Backward(1)),
-            query,
+            query: query.into(),
         }
     }
 
-    pub fn forward_until(query: String) -> Self {
+    pub fn forward_until<T: Into<String>>(query: T) -> Self {
         Self {
             step: LineCrossing::new(CharMotion::Forward(1)),
-            query,
+            query: query.into(),
         }
     }
 }
@@ -44,18 +44,19 @@ impl Motion for SearchMotion {
 
         loop {
             let (next_cursor, found) = search(context, cursor, &self.step, |c| c == first_char);
-            if !found {
+            if !found || next_cursor == cursor {
                 return origin;
             }
 
             cursor = next_cursor;
             let line = context.buffer().get(cursor.line);
-            if line.width() < cursor.col + self.query.len() {
+            let end = cursor.col + self.query.len();
+            if line.width() < end {
                 // Cannot possibly be a match
                 continue;
             }
 
-            let candidate = line.subs(cursor.col, self.query.len());
+            let candidate = line.subs(cursor.col, end);
             if candidate.starts_with(&self.query) {
                 // Found it! Return the cursor
                 break;
@@ -63,5 +64,53 @@ impl Motion for SearchMotion {
         }
 
         cursor
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::editing::motion::tests::window;
+    use indoc::indoc;
+
+    mod forward_search {
+        use super::*;
+
+        #[test]
+        fn within_line() {
+            let mut ctx = window(indoc! {"
+                |Take my land
+            "});
+            ctx.motion(SearchMotion::forward_until("my"));
+            ctx.assert_visual_match(indoc! {"
+                Take |my land
+            "});
+        }
+
+        #[test]
+        fn dont_move_without_match() {
+            let mut ctx = window(indoc! {"
+                |Take my land
+            "});
+            ctx.motion(SearchMotion::forward_until("alright"));
+            ctx.assert_visual_match(indoc! {"
+                |Take my land
+            "});
+        }
+    }
+
+    mod backward_search {
+        use super::*;
+
+        #[test]
+        fn within_line() {
+            let mut ctx = window(indoc! {"
+                Take my |land
+            "});
+            ctx.motion(SearchMotion::backward_until("my"));
+            ctx.assert_visual_match(indoc! {"
+                Take |my land
+            "});
+        }
     }
 }

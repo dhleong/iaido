d93b2668bf7c3293ad43a8d5e926c8467db5d9d9
Daniel Leong <me@dhleong.net>
2021-09-11 12:51:31 -0400

    Add limits to history size

--- src/input/history.rs
+++ src/input/history.rs
@@ -1,25 +1,42 @@
+use std::collections::vec_deque::Iter;
 use std::collections::HashMap;
+use std::collections::VecDeque;
+
+const DEFAULT_HISTORY_LIMIT: usize = 10000;
 
 pub struct History<T> {
-    pub items: Vec<T>,
+    items: VecDeque<T>,
+    pub limit: usize,
 }
 
 impl<T> History<T> {
+    pub fn with_limit(limit: usize) -> Self {
+        Self {
+            items: Default::default(),
+            limit,
+        }
+    }
+
     pub fn first(&self) -> Option<&T> {
         self.items.get(0)
     }
 
     pub fn insert(&mut self, item: T) {
-        // TODO History limits
-        self.items.insert(0, item)
+        while self.items.len() >= self.limit {
+            self.items.pop_back();
+        }
+        self.items.push_front(item);
+    }
+
+    #[allow(dead_code)] // NOTE: We use it in a test, and will need it later
+    pub fn iter(&self) -> Iter<T> {
+        self.items.iter()
     }
 }
 
 impl<T> Default for History<T> {
     fn default() -> Self {
-        Self {
-            items: Default::default(),
-        }
+        Self::with_limit(DEFAULT_HISTORY_LIMIT)
     }
 }
 
@@ -52,5 +69,20 @@ impl StringHistories {
         }
 
         history.insert(entry)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn history_limit() {
+        let mut history = History::<&str>::with_limit(2);
+        history.insert("First");
+        history.insert("Second");
+        history.insert("Third");
+        let contents: Vec<String> = history.iter().map(|s| s.to_string()).collect();
+        assert_eq!(contents, vec!["Third", "Second"]);
     }
 }

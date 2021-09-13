b4be504d7ee2bcfc84d671fa8079517e912fb55c
Daniel Leong <me@dhleong.net>
2021-09-13 10:40:58 -0400

    Add version and a `:connect` hint to the splash

--- src/tui/splash.rs
+++ src/tui/splash.rs
@@ -1,4 +1,6 @@
+use clap::crate_version;
 use tui::layout::Rect;
+use tui::style::Modifier;
 use tui::widgets::Paragraph;
 use tui::widgets::Widget;
 use tui::{
@@ -11,20 +13,34 @@ use super::rendering::display::Display;
 use crate::editing::Size;
 
 pub fn render(display: &mut Display) {
-    let Size { w, h } = display.size;
+    let var_style = Style::default().add_modifier(Modifier::BOLD);
+    let enter_style = Style::default().fg(Color::Magenta);
 
-    let widget_height = 3;
-    let widget = Paragraph::new(vec![
+    // TODO Maybe we can simplify the alignment and formatting of these hints?
+    let lines = vec![
         Spans::from("iaido"),
+        Spans::from(format!("version {}", crate_version!())),
         Spans::from(""),
         Spans::from(vec![
-            Span::from("type  :q"),
-            Span::styled("<Enter>", Style::default().fg(Color::Magenta)),
-            Span::from("  to exit"),
+            Span::from("type  :connect "),
+            Span::styled("<host>", var_style),
+            Span::from(":"),
+            Span::styled("<port>", var_style),
+            Span::styled("<Enter>", enter_style),
+            Span::from("  to connect"),
         ]),
-    ])
-    .alignment(Alignment::Center);
+        Spans::from(vec![
+            Span::from("type  :q"),
+            Span::styled("<Enter>", enter_style),
+            Span::from("                      to exit   "),
+        ]),
+    ];
 
+    let widget_height = lines.len() as u16;
+    let widget = Paragraph::new(lines).alignment(Alignment::Center);
+
+    // Render vertically centered
+    let Size { w, h } = display.size;
     widget.render(
         Rect::new(0, h / 2 - widget_height / 2, w, widget_height),
         &mut display.buffer,

c10fa17a90c9f9e44b09b3d46f3406452e5e22f3
Daniel Leong <me@dhleong.net>
2021-11-27 13:48:41 -0500

    Implement prompt history navigation (#68)
    * Add *rough* initial (single-item!) history recall
    
    * Implement step-wise history navigation!
    
    * Make the HistoryCursor API more symmetrical
    
    This is a bit more complicated (and we can't clear out the
    stashed_input, since we have to return a ref) but it's a much more sane
    API!
    
    * Implement prefix-filtered history navigation

--- src/input/history.rs
+++ src/input/history.rs
@@ -18,7 +18,11 @@ impl<T> History<T> {
     }
 
     pub fn first(&self) -> Option<&T> {
-        self.items.get(0)
+        self.nth(0)
+    }
+
+    pub fn nth(&self, n: usize) -> Option<&T> {
+        self.items.get(n)
     }
 
     pub fn insert(&mut self, item: T) {
@@ -52,8 +56,18 @@ impl StringHistories {
             .or_insert_with(|| Default::default())
     }
 
-    pub fn get_most_recent(&self, key: String) -> Option<&String> {
-        if let Some(history) = self.histories.get(&key) {
+    pub fn take(&mut self, key: &String) -> History<String> {
+        self.histories
+            .remove(key)
+            .unwrap_or_else(|| Default::default())
+    }
+
+    pub fn replace(&mut self, key: String, history: History<String>) {
+        self.histories.insert(key, history);
+    }
+
+    pub fn get_most_recent(&self, key: &str) -> Option<&String> {
+        if let Some(history) = self.histories.get(key) {
             history.first()
         } else {
             None
@@ -61,6 +75,7 @@ impl StringHistories {
     }
 
     pub fn maybe_insert(&mut self, key: String, entry: String) {
+        // TODO This should actually *remove* older matching entries, per :help cmdline-history
         let history = self.get(key);
         if let Some(existing) = history.first() {
             if existing.to_owned() == entry {
@@ -69,6 +84,81 @@ impl StringHistories {
         }
 
         history.insert(entry)
+    }
+}
+
+pub trait HistoryCursorable {
+    fn filter_history(&self, candidate: &Self) -> bool;
+}
+
+impl HistoryCursorable for String {
+    fn filter_history(&self, candidate: &String) -> bool {
+        (&self[..]).filter_history(&&candidate[..])
+    }
+}
+
+impl HistoryCursorable for &str {
+    fn filter_history(&self, candidate: &&str) -> bool {
+        candidate.starts_with(self)
+    }
+}
+
+#[derive(Default)]
+pub struct HistoryCursor<T: HistoryCursorable> {
+    index: Option<usize>,
+    stashed_input: Option<T>,
+}
+
+impl<T: HistoryCursorable> HistoryCursor<T> {
+    pub fn back<'h, 'a: 'h>(
+        &mut self,
+        stash_input: impl Fn() -> T,
+        history: &'h History<T>,
+    ) -> Option<&'h T> {
+        let next_index = if let Some(previous_index) = self.index {
+            previous_index + 1
+        } else {
+            self.stashed_input = Some(stash_input());
+            0
+        };
+
+        let mut iter = history
+            .iter()
+            .filter(|candidate| self.filter_history(candidate))
+            .skip(next_index);
+        if let Some(next) = iter.next() {
+            self.index = Some(next_index);
+            Some(next)
+        } else {
+            None
+        }
+    }
+
+    pub fn forward<'h, 'a: 'h>(&'a mut self, history: &'h History<T>) -> Option<&'h T> {
+        if let Some(previous_index) = self.index {
+            if previous_index > 0 {
+                let new_index = previous_index - 1;
+                self.index = Some(new_index);
+                history
+                    .iter()
+                    .filter(|candidate| self.filter_history(candidate))
+                    .nth(new_index)
+            } else {
+                self.index = None;
+                self.stashed_input.as_ref()
+            }
+        } else {
+            None
+        }
+    }
+
+    fn filter_history(&self, candidate: &T) -> bool {
+        // NOTE: If stashed_input is not empty, vim searches for a prefix match:
+        if let Some(stashed) = self.stashed_input.as_ref() {
+            stashed.filter_history(candidate)
+        } else {
+            true
+        }
     }
 }
 
@@ -84,5 +174,73 @@ mod tests {
         history.insert("Third");
         let contents: Vec<String> = history.iter().map(|s| s.to_string()).collect();
         assert_eq!(contents, vec!["Third", "Second"]);
+    }
+
+    mod cursor_tests {
+        use super::*;
+
+        #[test]
+        fn navigate_simple() {
+            let mut history = History::<&str>::with_limit(2);
+            history.insert("First");
+            history.insert("Second");
+
+            let stash_input = || "";
+
+            let mut cursor = HistoryCursor::<&str>::default();
+            let entry = cursor.back(stash_input, &history);
+            assert_eq!(entry.unwrap().to_owned(), "Second");
+
+            let entry = cursor.back(stash_input, &history);
+            assert_eq!(entry.unwrap().to_owned(), "First");
+
+            // No more history returns None
+            assert_eq!(cursor.back(stash_input, &history), None);
+
+            let entry = cursor.forward(&history);
+            assert_eq!(entry.unwrap().to_owned(), "Second");
+
+            // We had no input in the prompt, so we get a blank string back:
+            let entry = cursor.forward(&history);
+            assert_eq!(entry.unwrap().to_owned(), "");
+
+            // And no further to go:
+            assert_eq!(cursor.forward(&history), None);
+            assert_eq!(cursor.forward(&history), None);
+
+            // If we go back again with new content...
+            let stash_input = || "Sec";
+            let entry = cursor.back(stash_input, &history);
+            assert_eq!(entry.unwrap().to_owned(), "Second");
+
+            // ... we should return to the new content
+            let entry = cursor.forward(&history);
+            assert_eq!(entry.unwrap().to_owned(), "Sec");
+        }
+
+        #[test]
+        fn navigate_filtered() {
+            let mut history = History::<&str>::with_limit(2);
+            history.insert("First");
+            history.insert("Second");
+
+            let stash_input = || "Fir";
+
+            let mut cursor = HistoryCursor::<&str>::default();
+            let entry = cursor.back(stash_input, &history);
+            assert_eq!(entry.unwrap().to_owned(), "First");
+
+            // No more filtered history returns None
+            assert_eq!(cursor.back(stash_input, &history), None);
+            assert_eq!(cursor.back(stash_input, &history), None);
+
+            // Skip back over Second to our stashed input
+            let entry = cursor.forward(&history);
+            assert_eq!(entry.unwrap().to_owned(), "Fir");
+
+            // And no further to go:
+            assert_eq!(cursor.forward(&history), None);
+            assert_eq!(cursor.forward(&history), None);
+        }
     }
 }
--- src/input/maps/vim/prompt.rs
+++ src/input/maps/vim/prompt.rs
@@ -1,10 +1,12 @@
 use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
 use crate::{
     editing::text::EditableLine,
     input::{
         commands::{CommandHandler, CommandHandlerContext},
         completion::Completer,
+        history::HistoryCursor,
         KeyCode, KeymapContext,
     },
     key_handler, vim_tree,
@@ -14,6 +16,7 @@ use super::{insert::vim_insert_mappings, tree::KeyTreeNode, VimKeymap, VimMode};
 
 pub struct VimPromptConfig {
     pub prompt: String,
+    pub history_key: String,
     pub handler: Box<CommandHandler>,
     pub completer: Option<Rc<dyn Completer>>,
 }
@@ -58,16 +61,56 @@ impl Into<VimMode> for VimPromptConfig {
 }
 
 fn mappings(config: VimPromptConfig) -> KeyTreeNode {
+    let up_prompt = config.prompt.to_string();
+    let down_prompt = config.prompt.to_string();
     let prompt_len = config.prompt.len();
+    let up_history_key = config.history_key.to_string();
+    let down_history_key = config.history_key.to_string();
+
+    let cursor = Arc::new(Mutex::new(HistoryCursor::<String>::default()));
+    let up_cursor = cursor.clone();
+    let down_cursor = cursor.clone();
+
     vim_tree! {
         "<esc>" => |ctx| {
             ctx.keymap.mode_stack.pop();
             ctx.state_mut().prompt.clear();
             Ok(())
-         },
+        },
+
+        "<up>" => move |ctx| {
+            let mut cursor = up_cursor.lock().unwrap();
+            let history = ctx.keymap.histories.take(&up_history_key);
+            let stash_input = || (&ctx.state().prompt.buffer.get_contents()[prompt_len..]).to_string();
+            if let Some(previous) = cursor.back(stash_input, &history) {
+                let mut new_content = String::new();
+                new_content.push_str(&up_prompt);
+                new_content.push_str(previous);
+
+                ctx.state_mut().prompt.activate(new_content.into());
+            }
+            ctx.keymap.histories.replace(up_history_key.to_string(), history);
+            Ok(())
+        },
+
+        "<down>" => move |ctx| {
+            let mut cursor = down_cursor.lock().unwrap();
+            let history = ctx.keymap.histories.take(&down_history_key);
+            if let Some(previous) = cursor.forward(&history) {
+                let mut new_content = String::new();
+                new_content.push_str(&down_prompt);
+                new_content.push_str(previous);
+
+                ctx.state_mut().prompt.activate(new_content.into());
+            }
+            ctx.keymap.histories.replace(down_history_key.to_string(), history);
+            Ok(())
+        },
 
          "<cr>" => move |ctx| {
              let input = ctx.state().prompt.buffer.get_contents()[prompt_len..].to_string();
+
+             ctx.keymap.histories.maybe_insert(config.history_key.to_string(), input.to_string());
              ctx.keymap.mode_stack.pop();
              ctx.state_mut().prompt.clear();
 

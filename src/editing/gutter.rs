ced90ceb285bd9b52a6fb4acd53988a2916bcf49
Daniel Leong <me@dhleong.net>
2021-11-28 16:03:10 -0500

    Render ~ in gutter of non-existent cmdline window lines

--- src/editing/gutter.rs
+++ src/editing/gutter.rs
@@ -2,5 +2,5 @@ use crate::editing::text::TextLine;
 
 pub struct Gutter {
     pub width: u8,
-    pub get_content: Box<dyn Fn(usize) -> TextLine>,
+    pub get_content: Box<dyn Fn(Option<usize>) -> TextLine>,
 }

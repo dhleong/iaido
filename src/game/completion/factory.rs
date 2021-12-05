cde6e16f4daf9f34b70419b2388a655e4c2af06d
Daniel Leong <me@dhleong.net>
2021-12-04 23:24:12 -0500

    Install a completion source

--- src/game/completion/factory.rs
+++ src/game/completion/factory.rs
@@ -1,4 +1,5 @@
 use super::multiplex::MultiplexCompletionSource;
+use super::recency::RecencyCompletionSource;
 use crate::game::completion::multiplex::weighted::WeightedRandomSelectorFactory;
 use crate::game::completion::CompletionSource;
 
@@ -6,8 +7,10 @@ pub struct GameCompletionsFactory;
 
 impl GameCompletionsFactory {
     pub fn create() -> MultiplexCompletionSource<Box<dyn CompletionSource>> {
+        // TODO: Distinguish completion processing between sent and received
+        let received_source = RecencyCompletionSource::default();
         MultiplexCompletionSource {
-            sources: vec![],
+            sources: vec![Box::new(received_source)],
             selector_factory: Box::new(WeightedRandomSelectorFactory::with_weights(vec![100])),
         }
     }

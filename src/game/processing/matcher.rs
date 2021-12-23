09a053d2676e8abea3ec791d3a50ef8b1aa26fee
Daniel Leong <me@dhleong.net>
2021-12-23 08:50:41 -0500

    Add initial simple alias compilation

--- src/game/processing/matcher.rs
+++ src/game/processing/matcher.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use lazy_static::lazy_static;
 use regex::Regex;
 
 use crate::{
@@ -17,15 +18,20 @@ impl Matcher {
     pub fn compile(input: String) -> KeyResult<Matcher> {
         if input.starts_with("/") && input.ends_with("/") {
             let pattern = input.trim_matches('/');
-            match Regex::new(pattern) {
-                Ok(regex) => Ok(Matcher {
-                    description: input,
-                    regex,
-                }),
-                Err(e) => Err(KeyError::InvalidInput(e.to_string())),
-            }
+            Self::from_pattern(&input, pattern)
         } else {
-            todo!("translate simple pattern");
+            let pattern = simple_matcher_to_pattern(&input);
+            Self::from_pattern(&input, &pattern)
+        }
+    }
+
+    fn from_pattern<D: Into<String>>(description: D, pattern: &str) -> KeyResult<Matcher> {
+        match Regex::new(pattern) {
+            Ok(regex) => Ok(Matcher {
+                description: description.into(),
+                regex,
+            }),
+            Err(e) => Err(KeyError::InvalidInput(e.to_string())),
         }
     }
 
@@ -34,7 +40,8 @@ impl Matcher {
             let mut result = Match::empty();
             for (i, name) in self.regex.capture_names().enumerate() {
                 let (key, captured) = if let Some(name) = name {
-                    (name.to_string(), captures.name(name))
+                    let clean_name = name.trim_start_matches("_VAR_");
+                    (clean_name.to_string(), captures.name(name))
                 } else {
                     (i.to_string(), captures.get(i))
                 };
@@ -48,6 +55,41 @@ impl Matcher {
         }
         None
     }
+}
+
+/// Given a string representing a simple input spec (that is, $1/$2 and ${name}-style variable
+/// placeholders and no regex except for `^`), compile a regex pattern
+fn simple_matcher_to_pattern(input: &str) -> String {
+    lazy_static! {
+        static ref VAR_REGEX: Regex = Regex::new(r"\$(\d+|\{\w+\})").unwrap();
+    }
+
+    let mut p = String::new();
+    let starts_at_beginning = input.starts_with('^');
+    let mut last_end = if starts_at_beginning { 1 } else { 0 };
+
+    if starts_at_beginning {
+        // NOTE: We do this here to avoid the ^ getting escaped below
+        p.push('^');
+    }
+
+    for m in VAR_REGEX.find_iter(&input) {
+        p.push_str(&regex::escape(&input[last_end..m.start()]));
+        last_end = m.end();
+
+        let var_name_range = if &input[m.start() + 1..m.start() + 2] == "{" {
+            // Named var
+            m.start() + 2..m.end() - 1
+        } else {
+            // Simple number
+            m.start() + 1..m.end()
+        };
+        p.push_str("(?P<_VAR_");
+        p.push_str(&input[var_name_range]);
+        p.push_str(">[^ ]+)");
+    }
+
+    p
 }
 
 pub struct Match {
@@ -101,6 +143,29 @@ mod tests {
                 .find("saute peppers".into())
                 .expect("Failed to match");
             assert_eq!(m.group("food").unwrap().to_string(), "peppers");
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn indexed() {
+            let matcher = re_compile(r"^saute $0");
+            let m = matcher
+                .find("saute peppers".into())
+                .expect("Failed to match");
+            assert_eq!(group(m, "0"), "peppers");
+        }
+
+        #[test]
+        fn named() {
+            let matcher = re_compile(r"^saute ${food}");
+            let m = matcher
+                .find("saute peppers".into())
+                .expect("Failed to match");
+            assert_eq!(group(m, "food"), "peppers");
         }
     }
 }
